3a169ca1305a182f628612effce408b15a79a170
Seda Ünal <sedaylmz06@outlook.com>
2022-11-03 17:20:04 +0300

    arra filter

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+drivers = ["seda", "aykut", "seda"];
+function findMatching(name) {
+  return name === "seda";
+}
+const filtered = drivers.filter(findMatching);
+//console.log(filtered);
+
+// findMatching(drivers)[(1, 2, 3, 4, 5)].filter(function (num) {
+//   return num > 3;
+// });
+
+function fuzzyMatch(name) {
+  return name.toLowerCase().indexOf("a") === 0;
+}
+let startsWithA = drivers.filter(fuzzyMatch);
+//console.log(startsWithA);
+
+driver = [
+  { name: "seda", hometown: "ankara" },
+  { name: "aykut", hometown: "ankara" },
+];
+
+function matchName(collection) {
+  for (const driver of collection) {
+    if (driver.name === "seda") {
+      console.log(driver);
+    }
+  }
+}
+matchName(driver);
